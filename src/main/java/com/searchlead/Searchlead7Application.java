b05d158feb0cd08d6bf6593c912d2762ae9244ce
Coding8008 <prasadpoojar63@gmail.com>
2022-12-06 20:34:55 +0530

    searchlead7 backup

---
+++ src/main/java/com/searchlead/Searchlead7Application.java
@@ -0,0 +1,13 @@
+package com.searchlead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Searchlead7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Searchlead7Application.class, args);
+	}
+
+}
